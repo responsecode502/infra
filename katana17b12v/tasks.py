56675b7290f4b26a8c028ca5f1e8ef69d2efb1b1
responsecode502 <responsecode502@gmail.com>
2026-06-17 12:59:58 +0300

    lets test

--- katana17b12v/tasks.py
+++ katana17b12v/tasks.py
@@ -1,9 +1,12 @@
 import os
 import tomllib
+import structlog
 from pathlib import Path
 from dotenv import load_dotenv
 from invoke import task
 from invoke.exceptions import Exit
+
+logger = structlog.get_logger()
 
 BASE_DIR = Path(__file__).parent
 
@@ -18,6 +21,13 @@ TEMPLATES = config["templates"]
 
 load_dotenv(dotenv_path = BASE_DIR / ".env")
 
-@task()
+@task
+def set_root_password(c):
+    if not (password := os.os.getenv("ROOT_PASSWORD")):
+        raise Exit("ROOT_PASSWORD missing", code = 1)
+    logger.info("root password updated")
+    c.config.update({"sudo" : {"password" : password}})
+
+@task(pre = [set_root_password])
 def setup_system(c):
     print("Hello from uv run inv setup-system")
