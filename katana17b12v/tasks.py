3b9bcb33c0ea45240c668f6a04c9a73f73971164
responsecode502 <responsecode502@gmail.com>
2026-06-17 15:05:15 +0300

    idk

--- katana17b12v/tasks.py
+++ katana17b12v/tasks.py
@@ -4,6 +4,7 @@ import structlog
 from pathlib import Path
 from dotenv import load_dotenv
 from invoke import task
+from invoke.watchers import Responder
 from invoke.exceptions import Exit
 
 logger = structlog.get_logger()
@@ -17,9 +18,25 @@ with open(config_path, "rb") as f:
     config = tomllib.load(f)
 
 HARDWARE = config["hardware"]
+REPOS = config["repos"]
+SYSTEM = config["system"]
 TEMPLATES = config["templates"]
 
+PART_EFI = HARDWARE["part_efi"]
+PART_ROOT = HARDWARE["part_root"]
+MNT = HARDWARE["mnt"]
+
+XBPS_REPO = REPOS["xbps"]
+
+HOSTNAME = SYSTEM["hostname"]
+TIMEZONE = SYSTEM["timezone"]
+
+FSTAB = TEMPLATES["fstab"]
+BTRBK = TEMPLATES["btrbk"]
+
 load_dotenv(dotenv_path = BASE_DIR / ".env")
+
+BOOTSTRAP_PACKAGES = ["base-system", "btrfs-progs", "grub-x86_64-efi", "os-prober"]
 
 @task
 def set_root_password(c):
@@ -28,6 +45,89 @@ def set_root_password(c):
     logger.info("root password updated")
     c.config.update({"sudo" : {"password" : password}})
 
-@task(pre = [set_root_password])
+@task(pre=[set_root_password])
+def do_partitioning(c):
+    logger.info("phase 1: partitioning", step = "start")
+    c.sudo(f"mkfs.btrfs -f {PART_ROOT}", pty=True)
+    c.sudo(f"mount {PART_ROOT} {MNT}")
+    for sv in ["@", "@home", "@snapshots"]:
+        c.sudo(f"btrfs subvolume create {MNT}/{sv}", pty=True)
+    c.sudo(f"umount {MNT}")
+    logger.info("phase 1: partitioning", step = "finish")
+
+@task(pre=[set_root_password])
+def do_mounting_layout(c):
+    logger.info("phase 2: mounting layout", step = "start")
+    options = "noatime,compress=zstd,ssd,discard=async"
+    mount_commands = [
+        f"mount -o {options},subvol=@ {PART_ROOT} {MNT}",
+        f"mkdir -p {MNT}/home {MNT}/.snapshots {MNT}/boot/efi {MNT}/mnt/btrfs-root",
+        f"mount -o {options},subvol=@home {PART_ROOT} {MNT}/home",
+        f"mount -o {options},subvol=@snapshots {PART_ROOT} {MNT}/.snapshots",
+        f"mount {PART_EFI} {MNT}/boot/efi"
+    ]
+    for cmd in mount_commands:
+        c.sudo(cmd, pty=True)
+    logger.info("phase 2: mounting layout", step = "finish") 
+
+@task(pre=[set_root_password])
+def do_bootstrap(c):
+    logger.info("phase 3: bootstrap OS core", step = "start")    
+    bootstrap_commands = [
+        f"mkdir -p {MNT}/var/db/xbps/keys",
+        f"cp -R /var/db/xbps/keys/* {MNT}/var/db/xbps/keys/",
+        f"xbps-install -R {XBPS_REPO} -r {MNT} {' '.join(BOOTSTRAP_PACKAGES)} --yes"
+    ]
+    for cmd in bootstrap_commands:
+        c.sudo(cmd, pty=True)
+    logger.info("phase 3: bootstrap OS core", step = "finish")
+
+@task(pre=[set_root_password])
+def do_chroot(c):
+    logger.info("phase 4: chroot", step = "start")
+    c.sudo(f"mount --make-rslave {MNT}")
+    for fs in ["dev", "proc", "sys", "run"]:
+        c.sudo(f"mount --rbind /{fs} {MNT}/{fs}")
+    c.sudo(f"mount --rbind /sys/firmware/efi/efivars {MNT}/sys/firmware/efi/efivars")
+    c.sudo(f"cp /etc/resolv.conf {MNT}/etc/")
+
+    in_chroot = f"chroot {MNT}"
+
+    c.sudo(f"{in_chroot} bash -c 'echo {HOSTNAME} > /etc/hostname'")
+    c.sudo(f"{in_chroot} bash -c 'echo \"LANG=en_US.UTF-8\" > /etc/locale.conf'")
+    c.sudo(f"{in_chroot} bash -c 'echo \"en_US.UTF-8 UTF-8\" >> /etc/default/libc-locales'")
+    c.sudo(f"{in_chroot} xbps_reconfigure -f glibc-locales", pty=True)
+    c.sudo(f"{in_chroot} ln -sf /usr/share/zoneinfo/{TIMEZONE} /etc/localtime")
+
+    _response = f"{os.getenv('ROOT_PASSWORD')}\n"
+    pass_responder = Responder(pattern=r"New password:", response=_response)
+    retry_responder = Responder(pattern=r"Retype new password:", response=_response)
+
+    c.sudo(
+        f"chroot {MNT} passwd root", 
+        watchers=[pass_responder, retry_responder], 
+        pty=True
+    )
+
+    root_uuid = os.popen(f"blkid -o value -s UUID {PART_ROOT}").read().strip()
+    efi_uuid = os.popen(f"blkid -o value -s UUID {PART_EFI}").read().strip()
+    fstab_text = FSTAB.format(root_uuid=root_uuid, efi_uuid=efi_uuid)
+    Path(f"{MNT}/etc/fstab").write_text(fstab_text)
+
+    c.sudo(f"{in_chroot} bash -c 'echo \"GRUB_DISABLE_OS_PROBER=false\" >> /etc/default/grub'")
+    c.sudo(f"{in_chroot} grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=void --recheck", pty=True)
+    c.sudo(f"{in_chroot} xbps-reconfigure -fa", pty=True)
+    c.sudo(f"umount -R {MNT}")
+    
+    logger.info("phase 4: chroot", step = "finish")
+
+
+@task(pre = [
+    #  set_root_password,
+    #  do_partitioning,
+    #  do_mounting_layout,
+    #  do_bootstrap,
+    #  do_chroot,
+  ])
 def setup_system(c):
-    print("Hello from uv run inv setup-system")
+    pass
